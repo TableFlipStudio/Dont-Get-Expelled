ec8305b36a3f43d0aa86bc8ee3519b0eed4ea65c
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2021-12-04 15:25:32 +0100

    unpacking the folder

--- dont_get_expelled/settings.py
+++
@@ -1,7 +0,0 @@
-class Settings():
-    """Klasa przechowująca ustawienia gry"""
-
-    def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 750
-        self.bg_color = (255, 255, 255)
