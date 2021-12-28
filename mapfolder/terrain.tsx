e131e49a0321f52908fbab07ef6ac1840bc8fda0
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2021-12-28 13:41:05 +0100

    files

---
+++ mapfolder/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="C:\Users\iglis\OneDrive\Dokumenty\GitHub\Dont-Get-Expelled\mapfolder\terrain.png" width="1024" height="1024"/>
+</tileset>
