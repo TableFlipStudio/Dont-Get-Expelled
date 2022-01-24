e5e197da8646d7aaae96e53443761d583cb43904
DELTA-22 <piotrkud.2005@wp.pl>
2022-01-24 18:14:25 +0100

    Dialogue window now takes input and displays it in tab

--- dialogues.py
+++ dialogues.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.freetype
 
 class DialogueWindow():
     """Klasa odpowiadająca za okno dialogowe"""
@@ -19,8 +20,21 @@ class DialogueWindow():
             self.settings.tab_Ypos, self.settings.tab_width,
             self.settings.tab_height)
         self.tab_color = (255, 255, 255)
+        self.text_color = (0, 0, 0)
+        self.font = pygame.freetype.SysFont(None, 16)
+
+        self._prep_msg('Some text')
+
+    def _prep_msg(self, msg):
+        """Utworzenie obrazu tekstu do wyświetlenia"""
+        msg_pos = (self.tab_rect.x, self.tab_rect.y)
+        self.msg_image, self.msg_rect = self.font.render(msg)
+        self.msg_rect.x = self.tab_rect.x
+        self.msg_rect.y = self.tab_rect.y
 
     def blit_window(self):
-        """Wyświetlenie okna dialogowego i pola tekstowego na ekranie"""
+        """Wyświetlenie okna dialogowego, pola tekstowego
+        i kwestii na ekranie"""
         pygame.draw.rect(self.screen, self.color, self.rect)
         pygame.draw.rect(self.screen, self.tab_color, self.tab_rect)
+        self.screen.blit(self.msg_image, self.msg_rect)
