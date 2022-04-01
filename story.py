b0f9e6968566f6a5d12a8706f5f604c54241d200
DELTA-22 <piotrkud.2005@wp.pl>
2022-04-01 21:40:15 +0200

    After doing math Zyzio teleports before classroom

--- story.py
+++ story.py
@@ -8,9 +8,10 @@ class StoryEvents():
         self.map = dogex.map
         self.window = dogex.window
         self.character = dogex.character
+        self.dogex = dogex
 
         # List of quest codenames and index to decide, which quest is active
-        self.quests = ['math', None]
+        self.quests = ['math', 'zyzio', None]
         self.inx = 0
 
     def _check_story_events(self):
@@ -25,3 +26,8 @@ class StoryEvents():
                 self.window.node = self.window.dialogues['matma']
                 self.window.load_dialogue()
                 self.inx += 1
+
+        elif self.quests[self.inx] == 'zyzio':
+            encounter = self.map._access_Object('objects.zyzioencounter')
+            zyzio_obj = self.map._access_Object('npc.kuba')
+            zyzio_obj.x, zyzio_obj.y = encounter.x, encounter.y
