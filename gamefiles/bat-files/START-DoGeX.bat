a31d3e6399b861561dbd6c0d6fe6f695b3adba95
FoxjustFox <iglis.com20@gmail.com>
2022-04-08 22:39:07 +0200

    fixed a bug

--- gamefiles/bat-files/START-DoGeX.bat
+++ gamefiles/bat-files/START-DoGeX.bat
@@ -5,11 +5,11 @@ python -m pip install pytmx
 
 cd /d %USERPROFILE%\Documents\Dont-Get-Expelled\gamefiles
 
-move bat-files\RUN-Dont-Get-Expelled.bat %USERPROFILE%\Desktop\DoGeX
+move bat-files\RUN-Dont-Get-Expelled.bat %USERPROFILE%\Desktop\Dont-Get-Expelled
 
 cd /d %USERPROFILE%\Desktop\Dont-Get-Expelled
 
-start RUN-Dont-Get-Expelled.bat && del /Q %USERPROFILE%\Desktop\Dont-Get-Expelled\START-DoGeX.bat
+start RUN-DoGeX.bat && del /Q %USERPROFILE%\Desktop\Dont-Get-Expelled\START-DoGeX.bat
 
 exit
 
