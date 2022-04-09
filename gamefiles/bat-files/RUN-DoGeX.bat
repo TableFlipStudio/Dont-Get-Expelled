5cf13ee819b9f7566dbbe31e3f4abd4aaadab273
FoxjustFox <iglis.com20@gmail.com>
2022-04-09 14:34:19 +0200

    versioning

--- gamefiles/bat-files/RUN-DoGeX.bat
+++ gamefiles/bat-files/RUN-DoGeX.bat
@@ -1,6 +1,6 @@
 @echo off
 
-curl https://api.github.com/repos/TabeFlipStudio/Dont-Get-Expelled/gamefiles/version.txt %USERPROFILE%\Documents\Dont-Get-Expelled/temporary\version.txt | grep "browser_download_url.*exe" | cut -d : -f 2,3 | tr -d \" | wget -i -
+curl https://raw.githubusercontent.com/TabeFlipStudio/Dont-Get-Expelled/testy/gamefiles/version.txt -o %USERPROFILE%\Documents\Dont-Get-Expelled/temporary\version.txt 
 
 set /p origin=< %USERPROFILE%\Documents\Dont-Get-Expelled\temporary\version.txt
 # TODO: this the downloaded file
@@ -9,7 +9,7 @@ set /p origin=< %USERPROFILE%\Documents\Dont-Get-Expelled\temporary\version.txt
 set /p local=< %USERPROFILE%\Documents\Dont-Get-Expelled\gamefiles\version.txt
 
 if %mytextfile% == %mytextfile2% (
-echo "You have the latest version"
+    echo "You have the latest version"
 )
 
 if %origin% > %local% (
@@ -17,6 +17,31 @@ if %origin% > %local% (
     echo.
     echo Downloading...
 
+    rmdir /Q /s %USERPROFILE%\Documents\Dont-Get-Expelled
+    
+    mkdir %USERPROFILE%\Documents\Dont-Get-Expelled
+
+    curl -L https://github.com/TabeFlipStudio/Dont-Get-Expelled/archive/refs/heads/testy.zip -o %USERPROFILE%\Documents\Dont-Get-Expelled\Dont-Get-Expelled.zip
+
+    powershell expand-archive %USERPROFILE%\Documents\Dont-Get-Expelled\Dont-Get-Expelled.zip %USERPROFILE%\Documents\Dont-Get-Expelled\
+
+    cd /d %USERPROFILE%\Desktop
+
+    cd /d %USERPROFILE%\Documents\Dont-Get-Expelled
+
+    del /Q /F Dont-Get-Expelled.zip
+
+    move Dont-Get-Expelled-testy\gamefiles %USERPROFILE%\Documents\Dont-Get-Expelled\
+
+    move gamefiles\bat-files\START-DoGeX.bat %USERPROFILE%\Desktop\
+
+    move gamefiles\bat-files\uninstall-DoGeX.bat %USERPROFILE%\Documents\Dont-Get-Expelled\
+
+    mkdir %USERPROFILE%\Documents\Dont-Get-Expelled\temporary
+
+    rmdir /Q /s Dont-Get-Expelled-testy
+
+    cls
 
 )
     
