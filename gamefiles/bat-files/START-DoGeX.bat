e87bb646b06334b4e178d90b18bfbe09cf0a67ef
FOXjustFOX <87518349+FOXjustFOX@users.noreply.github.com>
2022-04-11 09:12:43 +0200

    update START-DoGeX.bat

--- gamefiles/bat-files/START-DoGeX.bat
+++ gamefiles/bat-files/START-DoGeX.bat
@@ -7,6 +7,11 @@ cd /d %USERPROFILE%\Documents\Dont-Get-Expelled\gamefiles
 
 move bat-files\RUN-DoGeX.bat %USERPROFILE%\Desktop
 
+echo.
+echo The START-DoGeX file was replaced by a RUN-DoGeX file on your Desktop
+echo.
+pause
+
 cd /d %USERPROFILE%\Desktop
 
 start RUN-DoGeX.bat & del /Q %USERPROFILE%\Desktop\START-DoGeX.bat & exit /B
